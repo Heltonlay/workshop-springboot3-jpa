9cac5c6b836ccfca272e871a36eab7c18ce75712
heltonlay <heltonlay15@gmail.com>
2024-10-01 20:38:19 -0300

    User insert

--- src/main/java/com/danielpsilva/course/services/CategoryService.java
+++ src/main/java/com/danielpsilva/course/services/CategoryService.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.danielpsilva.course.entities.Category;
-import com.danielpsilva.course.entities.User;
 import com.danielpsilva.course.repositories.CategoryRepository;
 
 @Service
