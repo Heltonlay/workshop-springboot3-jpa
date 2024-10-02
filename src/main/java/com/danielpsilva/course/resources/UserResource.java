3a1b1cda724bb689c22f882e06c75dd0d260ab4f
Heltonlay <heltonlay15@gmail.com>
2024-10-02 10:19:33 -0300

    User delete

--- src/main/java/com/danielpsilva/course/resources/UserResource.java
+++ src/main/java/com/danielpsilva/course/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,11 +35,17 @@ public class UserResource {
 		User user = userService.findById(id);
 		return ResponseEntity.ok().body(user);
 	}
-	
+
 	@PostMapping
-	public ResponseEntity<User> insert(@RequestBody User obj){
+	public ResponseEntity<User> insert(@RequestBody User obj) {
 		userService.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).body(obj);
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		userService.deleteById(id);
+		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/com/danielpsilva/course/services/UserService.java
+++ src/main/java/com/danielpsilva/course/services/UserService.java
@@ -11,20 +11,24 @@ import com.danielpsilva.course.repositories.UserRepository;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	UserRepository userRepository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return userRepository.findAll();
 	}
-	
+
 	public User findById(Long id) {
 		Optional<User> obj = userRepository.findById(id);
 		return obj.get();
 	}
-	
+
 	public User insert(User obj) {
 		return userRepository.save(obj);
+	}
+
+	public void deleteById(Long id) {
+		userRepository.deleteById(id);
 	}
 }
